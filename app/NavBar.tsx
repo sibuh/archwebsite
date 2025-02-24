6d6df38ddb9055b43fc0c3e9cd1e894fab8fd4ac
sibuh <sibuhdesalew@gmail.com>
2025-02-24 03:38:49 +0300

    added style to NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,10 @@
-
-import { link } from "fs";
+'use client';
 import Link from "next/link";
 import {FiList } from "react-icons/fi";
+import classNames from "classnames";
+import { usePathname } from "next/navigation";
 const NavBar = () => {
+    const currentPath=usePathname()
     const links=[
         {
             href:"/landscape",
@@ -28,7 +30,12 @@ const NavBar = () => {
            
             <ul className="flex space-x-10">
             {links.map(link=>
-               <Link  key={link.href} href={link.href} className="text-stone-800 hover:text-red-600 transition-colors"> 
+               <Link  key={link.href} href={link.href} 
+               className={classNames({
+                'text-stone-800': link.href===currentPath,
+                'hover:text-red-600': link.href!==currentPath,
+                'transition-colors':true
+               })}> 
                  {link.label}
                </Link>
             )
