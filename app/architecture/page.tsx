179b20dc020bad9451436ce50ee1862ebb78e957
sibuh <sibuhdesalew@gmail.com>
2025-02-24 21:46:07 +0300

    added prisma

---
+++ app/architecture/page.tsx
@@ -0,0 +1,11 @@
+const Architecture = () => {
+    return (  
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-full p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <p>
+                This is Architecture page
+            </p>
+        </div>
+    );
+}
+ 
+export default Architecture;
---
+++ app/landscape/page.tsx
@@ -0,0 +1,9 @@
+const Landscape = () => {
+    return ( 
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-full p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <p>This is landscape page</p>
+        </div>
+     );
+}
+ 
+export default Landscape;
