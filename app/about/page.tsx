ba5e19473da6930f59cfd06d58cadbe3657498b9
Misganaw <misgandess@gmail.com>
2025-02-26 15:19:17 +0300

    added side bar

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+const About = () => {
+    return ( 
+        <div className="">
+            <p>
+                This about page
+            </p>
+        </div>
+     );
+}
+ 
+export default About;
---
+++ app/sidebar/page.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import classNames from "classnames";
+import Link from "next/link"
+import { usePathname } from "next/navigation";
+
+const SideBar = () => {
+    const currentPath=usePathname()
+    const links=[
+        {
+            href:"/about",
+            label:"about"
+        }, {
+            href:"/career",
+            label:"career"
+        }
+        , {
+            href:"/people",
+            label:"people"
+        }
+    ]
+    return ( 
+        <div className="">
+            <ul className="flex flex-col space-y-5 border-red-50">
+                {links.map(link=>
+                    <Link 
+                        key={link.href}
+                        href={link.href}
+                        className={classNames({
+                        'text-stone-800': link.href===currentPath,
+                        'hover:text-red-600': link.href!==currentPath,
+                        'transition-colors':true
+                        })}
+                    >
+                        {link.label}
+
+                    </Link>
+                )}
+                
+            </ul>
+            
+        </div>
+     );
+}
+ 
+export default SideBar;
