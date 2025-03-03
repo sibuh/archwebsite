771ff79581e75778b74d5c87ab5bacce10afe132
sibuh <sibuhdesalew@gmail.com>
2025-03-03 06:45:44 +0300

    updated footer

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,12 +1,63 @@
+import Link from "next/link";
+import {
+    FiTwitter,
+    FiLinkedin,
+    FiYoutube,
+    FiFacebook,
+    FiInstagram
+  } from "react-icons/fi";
 const Footer = () => {
+    const socialMedias=[
+        {
+            link:"https://www.facebook.com",
+            icon: <FiFacebook />,
+            label:"Facebook"
+        },
+        {
+            link:"https://www.google.com/",
+            icon: <FiTwitter />,
+            label:"Twitter"
+        },
+        {
+            link:"https://web.facebook.com/",
+            icon: <FiLinkedin />,
+            label:"Linkedin"
+        },
+        {
+            link:"https://www.instagram.com/",
+            icon: <FiInstagram />,
+            label:"Instagram"
+        },
+        {
+            link:"https://www.youtube.com/",
+            icon: <FiYoutube />,
+            label:"Youtube"
+        },
+    ]
     return ( 
-        <footer className="h-14 border-t items-center space-y-4">
-            <p className="px-8">Contacts</p>
-            <ul className="flex space-x-10  px-8">
-                <li>Facebook</li>
-                <li>Telegram</li>
-                <li>Instagram</li>
+        <footer className="flex flex-col justify-center space-y-7 h-40 bg-gray-800 text-white">
+            <div className="flex justify-center items-center">
+                <p >Our Contacts:</p>
+            </div>
+            <div className="flex justify-center items-center">
+            <ul className="flex space-x-10">
+                {
+                    socialMedias.map((media)=>
+                        <div key={media.link}>
+                            <p> {media.icon}</p>
+                            <Link href={media.link}>
+                            {media.label}
+                            </Link>
+                        </div>
+                        
+                    )
+                }
             </ul>
+            </div>
+            <div className="flex justify-center items-center">
+                <p>© 2025 My Website</p>
+            </div>
+            
         </footer>
      );
 }
