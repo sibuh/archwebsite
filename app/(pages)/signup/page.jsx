989d4977f44bab19bf731e95de22723deabb7e71
Misganaw <misgandess@gmail.com>
2025-03-04 17:40:41 +0300

    updated signup form

--- app/(pages)/signup/page.jsx
+++ app/(pages)/signup/page.jsx
@@ -3,35 +3,41 @@
  const Signup= () => {
     const handleChange=()=>{}
     return <div>
-                <form>
+                <form className="flex flex-col ">
                     <input
+                        className="w-full p-2 border rounded"
                         type="text"
                         placeholder="FirstName"
                         onChange={handleChange}
                      />
                       <input
+                        className="w-full p-2 border rounded"
                         type="text"
                         placeholder="LastName"
                         onChange={handleChange}
                      />
 
                     <input
+                        className="w-full p-2 border rounded"
                         type="text"
                         placeholder="Username"
                         onChange={handleChange}
                     /> 
 
                     <input
+                     className="w-full p-2 border rounded"
                      type="text"
                      placeholder="Password"
                      onChange={handleChange}
                     /> 
                     <input
+                    className="w-full p-2 border rounded"
                     type="text"
                     placeholder="Email"
                     onChange={handleChange}
                     />
                     <input
+                        className="w-half p-2 border rounded"
                         type="text"
                         placeholder="phone"
                         onChange={handleChange}
