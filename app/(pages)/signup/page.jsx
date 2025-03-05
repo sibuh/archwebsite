dfdec00bc0c47d5b634ffd185537f8438f0cd84f
Misganaw <misgandess@gmail.com>
2025-03-05 14:36:14 +0300

    added spinner to Register button

--- app/(pages)/signup/page.jsx
+++ app/(pages)/signup/page.jsx
@@ -1,48 +1,81 @@
 "use client";
+import {Button,Spinner} from "@radix-ui/themes"
+
+
+import { useState } from "react";
 
  const Signup= () => {
-    const handleChange=()=>{}
+    const[user,setUser]=useState<FormData>({
+        first_name:"",
+        last_name:"",
+        username:"",
+        password:"",
+        email:"",
+        phone:""
+    });
+    const [processing,setProcessing]=useState(false);
+
+    const handleSubmit=async (e)=>{
+        e.preventDefault();
+        setProcessing(true);
+        try{
+            const res =await fetch("/app/users/signup",{
+                method:'POST',
+                body:data
+            })
+            if (!res.ok) throw new Error("Registration failed")
+            alert("Registration Successfull")
+        }catch(err){
+            alert(err);
+        }finally{
+            setProcessing(false);
+        }
+    }
     return <div>
-                <form className="flex flex-col ">
+            {!processing&&
+                <form className="flex flex-col " onSubmit={handleSubmit}>
                     <input
                         className="w-full p-2 border rounded"
                         type="text"
                         placeholder="FirstName"
-                        onChange={handleChange}
+                        onChange={setUser({...user,first_name:e.target.value})}
                      />
                       <input
                         className="w-full p-2 border rounded"
                         type="text"
                         placeholder="LastName"
-                        onChange={handleChange}
+                        onChange={setUser({...user,last_name:e.target.value})}
                      />
 
                     <input
                         className="w-full p-2 border rounded"
                         type="text"
                         placeholder="Username"
-                        onChange={handleChange}
+                        onChange={setUser({...user,username:e.target.value})}
                     /> 
 
                     <input
                      className="w-full p-2 border rounded"
                      type="password"
                      placeholder="Password"
-                     onChange={handleChange}
+                     onChange={setUser({...user,password:e.target.value})}
                     /> 
                     <input
                     className="w-full p-2 border rounded"
                     type="email"
                     placeholder="Email"
-                    onChange={handleChange}
+                    onChange={setUser({...user,email:e.target.value})}
                     />
                     <input
                         className="w-half p-2 border rounded"
                         type="text"
                         placeholder="phone"
-                        onChange={handleChange}
+                        onChange={setUser({...user,phone:e.target.value})}
                     />
+                 <Button type="submit" disabled={processing}>Register {processing&&<Spinner />} </Button>
                 </form>
+                
+                }
            </div>
 }
 export default Signup
