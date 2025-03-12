d0625cbdd35a159cda7db448fd3de36fadd5d675
Misganaw <misgandess@gmail.com>
2025-03-12 13:43:04 +0000

    displayed images and video on separete divs

--- app/(pages)/architecture/page.tsx
+++ app/(pages)/architecture/page.tsx
@@ -35,49 +35,60 @@ export default function Architecture() {
  
 
   return (
-       <div className="flex flex-col justify-center mt-3">
-          {isLoading?  (<div className="mt-40"><LoadingThreeDotsJumping  /></div>) :<div className="grid grid-cols-1 justify-items-center space-y-6 p-10 pl-60">
-          <div>
-            <p className="font-extrabold font-sans">Projects Done</p>
-          </div>
-          <div className="mt-4" >
-              {data.map((project)=>
-                <div key={project.id} className=" flex justify-center m-10 ">
-                    <div className="font-sans w-80 ">
-                      <h2 className="font-bold">{project.name}</h2>
-                      <p>{project.description}</p>
-                      <p>{project.status}</p>
+        <>
+          {isLoading? (<div className="mt-40">
+            <LoadingThreeDotsJumping  />
+            </div>) :
+          <div className="grid grid-cols-1 justify-items-center space-y-6 p-10">
+            
+            <div>
+              <p className="font-extrabold font-sans">Projects Done</p>
+            </div>
+
+            {/* <div className="flex flex-col space-y-5" > */} 
+            
+            <ul className="space-y-2">
+          {data.map((project) => (
+            <li key={project.id} className="flex justify-center space-x-5 rounded-md bg-black p-4">
+              {/* Text Content */}
+              <div className="font-sans w-64 h-60 rounded-md text-white">
+                <h2 className="font-bold">{project.name}</h2>
+                <p>{project.description}</p>
+                <p>{project.status}</p>
+              </div>
+
+              {/* Image and Video Carousel */}
+              <div className="w-64 h-60 rounded-md">
+                <Carousel>
+                  {project.imagePaths.map((path) => (
+                    <div key={path} className="w-64 h-60">
+                      <Image
+                        className="rounded-md w-full h-full object-cover"
+                        src={path}
+                        alt="Project picture"
+                        width={256}
+                        height={240}
+                      />
                     </div>
-                    <div className="w-64 h-60 rounded-b-md">
-                      <Carousel>
-                        {project.imagePaths.map((path) => (
-                          <Image
-                            key={path}
-                            className="rounded-md w-full h-full object-cover"
-                            src={path}
-                            alt="Project picture"
-                            width={256}
-                            height={240} 
-                          />
-                        ))}
-                        <video
-                          className="rounded-md w-full h-full object-cover"
-                          src={project.videoPaths[0]}
-                          autoPlay
-                          loop
-                          muted
-                        />
-                      </Carousel>
-                    </div> 
-                </div>
-                )}  
-          </div>
+                  ))}
+                  {project.videoPaths.length > 0 && (
+                    <div className="w-64 h-60">
+                      <video
+                        className="rounded-md w-full h-full object-cover"
+                        src={project.videoPaths[0]}
+                        autoPlay
+                        loop
+                        controls
+                      />
+                    </div>
+                  )}
+                </Carousel>
+              </div>
+            </li>
+          ))}
+        </ul>
+
         </div>}
-       </div> 
-          
-        
-      
-  )
-}
+</>)}
 
  
