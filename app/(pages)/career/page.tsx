133388a6090ce502f87d19403125d746506afa14
sibuh <sibuhdesalew@gmail.com>
2025-03-21 23:58:11 +0300

    silly change

--- app/(pages)/career/page.tsx
+++ app/(pages)/career/page.tsx
@@ -4,7 +4,7 @@ const Career = () => {
             <h1>This Career page</h1>
             <p>
                 
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium delectus voluptate sequi repellat corrupti perferendis aut dolorum incidunt ut, cumque facilis adipisci, natus quae nam? Libero a dolore omnis aperiam?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. ate sequi repellat corrupti perferendis aut dolorum incidunt ut, cumque facilis adipisci, natus quae nam? Libero a dolore omnis aperiam?
             </p>
         </div>
      );
