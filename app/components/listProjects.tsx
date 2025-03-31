1fabfd7db66af40197897f6e5f8047d1a31081a7
nardoz <gnardos13@gmail.com>
2025-03-31 10:32:22 +0300

    add shadow

--- app/components/listProjects.tsx
+++ app/components/listProjects.tsx
@@ -5,6 +5,17 @@ import LoadingThreeDotsJumping from "@/app/components/LoadTreeDotsJamping"
 import "react-responsive-carousel/lib/styles/carousel.min.css"
 import { useCallback, useEffect, useRef, useState } from "react"
 
+// function scrollToCenter(element: HTMLElement) {
+//   const elementRect = element.getBoundingClientRect();
+//   const absoluteElementTop = elementRect.top + window.pageYOffset;
+//   const middle = absoluteElementTop - (window.innerHeight / 2) + (elementRect.height / 2);
+
+//   window.scrollTo({
+//     top: middle,
+//     behavior: 'smooth' // optional
+//   });
+// }
+
 export interface Project {
   id: string
   name: string
@@ -25,6 +36,7 @@ type ListProps = {
 const ListProjects = ({ isLoading, data }: ListProps) => {
   const [scale, setScale] = useState(1)
   const [open, setOpen] = useState<string[]>([])
+  const [active, setActive] = useState(false)
   const timeout = useRef(Date.now())
 
   const requestAnim = useCallback(
@@ -42,6 +54,7 @@ const ListProjects = ({ isLoading, data }: ListProps) => {
   useEffect(() => {
     addEventListener("wheel", function () {
       timeout.current = Date.now()
+      setActive(false)
       requestAnim()
     })
     return () => {
@@ -49,45 +62,64 @@ const ListProjects = ({ isLoading, data }: ListProps) => {
     }
   }, [requestAnim])
 
+  useEffect(() => {
+    const element = document.getElementById(open[0])
+    if (!element) return
+
+    element.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+      inline: "center",
+    })
+  }, [open])
+
   return (
-    <>
+    <div className="relative">
+      {/* {active && (
+        <div className="fixed inset-0 backdrop-blur-sm bg-slate-200 bg-opacity-80 z-[1]" />
+      )} */}
       {isLoading ? (
         <div className="mt-40 flex justify-center">
           <LoadingThreeDotsJumping />
         </div>
       ) : (
-        <div className="grid justify-items-center space-y-6 p-6 md:p-10">
+        <div className="justify-items-center space-y-6 py-6 md:pt-10 z-[2] ">
           <h2 className="font-extrabold font-sans text-xl md:text-2xl text-center">
             Projects Done
           </h2>
 
           <ul
             style={{ scale: scale, transition: "all 1s ease" }}
-            className="w-full h-auto max-w-5xl space-y-8 transition-transform duration-700"
+            className="w-full h-auto space-y-8 transition-transform duration-700 z-0"
           >
             {data.map((project) => (
               <li
                 key={project.id}
+                id={project.id}
                 onClick={(e) => {
-                  e.currentTarget.scrollIntoView({
-                    behavior: "smooth",
-                    block: "center",
-                  })
+                  setActive(true)
                   e.currentTarget.style.width = "100%"
-                  e.currentTarget.style.height = "70vh"
                   setOpen((prev: string[]) => {
                     const newArray = [...prev]
-                    newArray.splice(prev.indexOf(project.id), 1, project.id)
+                    newArray.splice(prev.indexOf(project.id), 1)
+                    newArray.unshift(project.id)
                     return newArray
                   })
                 }}
                 style={{
                   transition: "all .3s ease-out",
+                  position: "relative",
+                  ...(active &&
+                    open[0] == project.id && {
+                      boxShadow: "0 0 100px 10px rgba(150, 150, 150, 0.7)",
+                      backdropFilter: "blur(12px)",
+                      zIndex: 1000,
+                    }),
                 }}
-                className="flex flex-col md:flex-row items-center md:items-start gap-2 bg-white shadow-md rounded-md p-4 cursor-pointer overflow-auto"
+                className="w-fit mx-auto flex flex-col md:flex-row items-center md:items-start gap-2 bg-white shadow-lg rounded-md p-4 cursor-pointer overflow-auto"
               >
                 {/* Text Content */}
-                <div className="w-full h-auto md:w-1/2 space-y-2 flex-shrink-0">
+                <div className="w-1/4 space-y-2 flex-shrink-0">
                   <h3 className="font-bold text-lg">{project.name}</h3>
                   <p className="text-sm">{project.description}</p>
                   <span className="text-sm font-semibold text-green-700">
@@ -96,34 +128,36 @@ const ListProjects = ({ isLoading, data }: ListProps) => {
                 </div>
 
                 {/* Image and Video Carousel */}
-                <div className="w-full md:w-1/2 ">
-                  <div className="flex gap-1 items-center">
-                    {open.includes(project?.id) ? (
-                      project.imagePaths.map((path) => (
-                        <div key={path} className="w-full h-auto flex-shrink-0">
-                          <Image
-                            className="rounded-md w-full h-64 object-cover"
-                            src={path}
-                            alt="Project picture"
-                            width={600}
-                            style={{
-                              height: "65vh",
-                            }}
-                          />
-                        </div>
-                      ))
-                    ) : (
-                      <div className="w-full h-auto flex-shrink-0">
+                <div className="flex gap-1 items-center">
+                  {open.includes(project?.id) ? (
+                    project.imagePaths.map((path) => (
+                      <div
+                        key={path}
+                        className="relative w-[800px] h-[500px] overflow-hidden"
+                      >
                         <Image
-                          className="rounded-md w-full h-64 object-cover"
-                          src={project.imagePaths[0]}
+                          className="rounded-md"
+                          src={path}
                           alt="Project picture"
-                          width={400}
-                          height={256}
+                          fill={true}
+                          objectFit="contain"
                         />
                       </div>
-                    )}
-                    {project.videoPaths.length > 0 && (
+                    ))
+                  ) : (
+                    <div className="flex-shrink-0">
+                      <Image
+                        className="rounded-md w-full h-64 object-cover"
+                        src={project.imagePaths[0]}
+                        alt="Project picture"
+                        width={400}
+                        height={256}
+                      />
+                    </div>
+                  )}
+                  {project.videoPaths.length > 0 &&
+                    active &&
+                    open[0] == project.id && (
                       <div className="w-full">
                         <video
                           className="rounded-md w-full h-64 object-cover"
@@ -135,14 +169,13 @@ const ListProjects = ({ isLoading, data }: ListProps) => {
                         />
                       </div>
                     )}
-                  </div>
                 </div>
               </li>
             ))}
           </ul>
         </div>
       )}
-    </>
+    </div>
   )
 }
 
