2e227b7d0b2420983f696f2058c4e6daeb8acf52
Misganaw <misgandess@gmail.com>
2025-04-01 07:01:00 +0000

    read secret key from env file

--- app/lib/auth.js
+++ app/lib/auth.js
@@ -1,14 +1,13 @@
 import jwt from 'jsonwebtoken' 
-const SECRET_KEY='12345678901234567890123456789012'
 
 
 export  function generateToken(user){
-  return jwt.sign(user , SECRET_KEY, { expiresIn: "1h" });
+  return jwt.sign(user ,process.env.SECRET_KEY, { expiresIn: "1h" });
 }
 
 
 
 export function verifyToken(token){
-  return jwt.verify(token, SECRET_KEY)
+  return jwt.verify(token, process.env.SECRET_KEY)
 }
   
